0a419efb58512c3cb6a674b896f26f73a8c87e54
alexpaper <alessandro.carta@gmail.com>
2023-03-03 18:17:48 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const btnStartReading = document.querySelector('.btn-start-read');
+const btnStopReading = document.querySelector('.btn-stop-read');
+const speech = new SpeechSynthesisUtterance();
+const language = 'it-IT';
+let itVoices;
+
+speechSynthesis.onvoiceschanged = () => {
+    let voices = Array.from(speechSynthesis.getVoices());
+    // console.log(voices)
+    itVoices = voices.filter(voice => voice.lang === language);
+    // console.log(itVoices)
+};
+
+btnStartReading.onclick = () => {
+    speech.text = document.querySelector('.text').textContent;
+    speech.voice = itVoices[4];
+    speechSynthesis.speak(speech);
+};
+
+btnStopReading.onclick = () => {
+    speechSynthesis.cancel();
+};
